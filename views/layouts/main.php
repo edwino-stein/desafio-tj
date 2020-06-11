541fc77196dafbc4e32578c2f5f6fa37844d59d9
Edwino Stein <edwino.stein@gmail.com>
2020-06-10 20:42:57 -0400

    Adicionado estrutura básica do backend com o framework Yii 2

---
+++ views/layouts/main.php
@@ -0,0 +1,18 @@
+<?php
+    use yii\helpers\Html;
+?>
+
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+        <?= $content ?>
+    </body>
+</html>
