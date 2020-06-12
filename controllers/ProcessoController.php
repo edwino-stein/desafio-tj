1e518c16eaa6cc80b2e105deb6d49cc6cc63395a
Edwino Stein <edwino.stein@gmail.com>
2020-06-12 03:13:40 -0400

    Adicionado rota para criação de processos

--- controllers/ProcessoController.php
+++ controllers/ProcessoController.php
@@ -5,13 +5,30 @@ namespace app\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\web\Response;
+use yii\filters\VerbFilter;
 
 use app\models\Processo;
+use app\models\Magistrado;
+use app\models\Orgao;
+use app\models\Partes;
+use app\models\Testemunha;
 
 class ProcessoController extends Controller
 {
 
     public $enableCsrfValidation = false;
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'criar'  => ['POST'],
+                ],
+            ],
+        ];
+    }
 
     /**
      * Lista um resumo de todos os processos
@@ -33,6 +50,85 @@ class ProcessoController extends Controller
     public function actionInfo($id)
     {
         $processo = Processo::findOneById($id);
+        return $this->asJson($processo->asArray());
+    }
+
+    /**
+     * Cria um processo
+     * rota: POST /processo/criar
+     */
+    public function actionCriar()
+    {
+        //Pega a requisição
+        $request = Yii::$app->request;
+
+        // Define variaveis fora do try-catch
+        $processo = null;
+        $transaction = null;
+
+        try {
+            // Ler o corpo da requisição em JSON com os dados de entrada
+            $data = json_decode($request->getRawBody(), true);
+
+            //Busca os registros que serão necessarias para os relacionamentos
+            $juiz = Magistrado::findOneById(isset($data['juiz']) ? $data['juiz'] : 0);
+            $orgao = Orgao::findOneById(isset($data['orgao']) ? $data['orgao'] : 0);
+            $vara = $juiz->getVara();
+
+            //Cria uma instância de Processo
+            $processo = new Processo($vara);
+
+            //Define o juiz e o orgão do processo
+            $processo->setJuiz($juiz);
+            $processo->setOrgao($orgao);
+
+            //Define outros campos do processo
+            $processo->setAssunto(isset($data['assunto']) ? $data['assunto'] : '');
+            $processo->setFatos(isset($data['fatos']) ? $data['fatos'] : '');
+            $processo->setPedidos(isset($data['pedidos']) ? $data['pedidos'] : '');
+            $processo->setValor(isset($data['valor']) ? $data['valor'] : '');
+
+            //Parseia entrada para ler ou criar os registros das partes e testemunhas
+            $partes = Partes::parsePartes($data['partes']);
+            $testemunhas = Testemunha::parseTestemunhas($data['testemulhas']);
+
+            //Inicia uma trasação com o banco
+            $transaction = Processo::getDb()->beginTransaction();
+
+            // Se o autor ou o reu forem novos registros, guarda no banco
+            if($partes['autor']->getIsNewRecord()) $partes['autor']->save();
+            if($partes['reu']->getIsNewRecord()) $partes['reu']->save();
+
+            //Instância um objeto Partes, inicializa e salva
+            $partesObj = new Partes();
+            $partesObj->setAutor($partes['autor'])
+                      ->setReu($partes['reu'])
+                      ->setAutorAdvogado($partes['autor_adv'])
+                      ->setReuAdvogado($partes['reu_adv'])
+                      ->save();
+
+            //Define as partes do processo e salva o processo
+            $processo->setPartes($partesObj)
+                     ->save();
+
+            //Cria instâncias de Testemunha, relaciona com o processo e as salva
+            foreach ($testemunhas as $k => $t) {
+                $testemunhas[$k] = Testemunha::criarTestemunha($t, $processo);
+                $testemunhas[$k]->save();
+            }
+
+            //Salva a vara com a nova sequencia
+            $vara->save();
+
+            //Se tudo correu bem, confirma as alterações no banco
+            $transaction->commit();
+        } catch(\Throwable $e) {
+            //Se houve algum erro, faz rollback no banco
+            if($transaction != null) $transaction->rollBack();
+            throw $e;
+        }
+
+        //Retorna o novo processo como JSON
         return $this->asJson($processo->asArray());
     }
 }
