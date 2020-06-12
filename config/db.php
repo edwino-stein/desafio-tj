ebf6a582f015405c1ac490320d1153fba4947b7c
Edwino Stein <edwino.stein@gmail.com>
2020-06-12 02:48:41 -0400

    Configurando a conexão com banco de dados local para desenvolvimento

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=tj',
+    'username' => 'tj',
+    'password' => 'tj1234',
+    'charset' => 'utf8'
 ];
